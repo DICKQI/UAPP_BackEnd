26ed443ecd823a6a5efb9e55127b92d66b47d503
dickqi <dickqi87@gmail.com>
2019-12-10 19:11:41 +0800

    C 修改websocket路由

--- App/ChatRoom/routing.py
+++ App/ChatRoom/routing.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import *
 
 websocket_patterns = [
-    path('ws/room/<int:rid>/', ChatRoomBaseWebsocket),
+    path('ws/room/<int:rid>', ChatRoomBaseWebsocket),
 ]
